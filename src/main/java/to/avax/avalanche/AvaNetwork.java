a8a365edad11fa44e70194cb261f667419057fea
avaxbuildr <avaxbuildr@crypto.bi>
2023-09-13 06:21:02 -0300

    Began work on network selection

---
+++ src/main/java/to/avax/avalanche/AvaNetwork.java
@@ -0,0 +1,69 @@
+/*
+ *
+ * Crypto.bi AvalancheJ - An experimental (unofficial) Java library for interacting with the Avalanche Platform
+ * Copyright (C) 2023 REKTBuildr
+ *
+ * For more information, visit:
+ * https://crypto.bi
+ *
+ *
+ **/
+
+package to.avax.avalanche;
+
+public class AvaNetwork {
+
+    public static int network_id = 0;
+    String name;
+    int id;
+    String protocol;
+    int port;
+    String ip;
+    int networkId;
+    String url;
+    String explorerUrl;
+    String explorerSiteUrl;
+    boolean readonly;
+    boolean withCredentials = false;
+
+    public AvaNetwork(String name,
+                        String url,
+                        int networkId,
+                        String explorerUrl,
+                        String explorerSiteUrl,
+                        boolean readonly){
+        this.id = network_id++;
+        this.name = name;
+        this.explorerUrl = explorerUrl;
+        this.explorerSiteUrl = explorerSiteUrl;
+        this.protocol = "http";
+        this.port = 9650;
+        this.ip = "localhost";
+        this.url = url;
+        this.updateURL(url);
+        this.networkId = networkId;
+        this.readonly = readonly;
+    }
+
+    public void updateURL(String url) {
+        String[] split = url.split("://");
+        this.protocol = split[0];
+
+        if (split[1].indexOf(':') >= 0) {
+            String[] urlSplit = split[1].split(":");
+            String ip = urlSplit[0];
+            String port = urlSplit[1];
+
+            this.ip = ip;
+            this.port = Integer.parseInt(port);
+        } else {
+            this.ip = split[1];
+            if (this.protocol.equals("http")) {
+                this.port = 80;
+            } else {
+                this.port = 443;
+            }
+        }
+    }
+
+}
--- src/main/java/to/avax/avalanche/apis/avm/tx/UnsignedTx.java
+++ src/main/java/to/avax/avalanche/apis/avm/tx/UnsignedTx.java
@@ -1,3 +1,14 @@
+/*
+ *
+ * Crypto.bi AvalancheJ - An experimental (unofficial) Java library for interacting with the Avalanche Platform
+ * Copyright (C) 2023 REKTBuildr
+ *
+ * For more information, visit:
+ * https://crypto.bi
+ *
+ *
+ **/
+
 package to.avax.avalanche.apis.avm.tx;
 
 import to.avax.avalanche.apis.avm.BaseTx;
